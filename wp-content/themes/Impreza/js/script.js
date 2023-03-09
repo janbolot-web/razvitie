4fccda32a632d60fd20eee80ffc8a4800bb910f5
Janbolot B <janbolotcode@gmail.com>
2023-03-09 16:54:30 +0600

    form

--- wp-content/themes/Impreza/js/script.js
+++ wp-content/themes/Impreza/js/script.js
@@ -19,3 +19,33 @@ var swiper1 = new Swiper(".mySwiper1", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+jQuery(document).ready(function () {
+     
+ jQuery('form button').click( function() {
+   var form = jQuery(this).closest('form');
+   
+   if ( form.valid() ) {
+     form.css('opacity','.5');
+     var actUrl = form.attr('action');
+
+     jQuery.ajax({
+       url: actUrl,
+       type: 'post',
+       dataType: 'html',
+       data: form.serialize(),
+       success: function(data) {
+         form.html(data);
+         form.css('opacity','1');
+                 //form.find('.status').html('форма отправлена успешно');
+                 //$('#myModal').modal('show') // для бутстрапа
+       },
+       error:	 function() {
+            form.find('.status').html('серверная ошибка');
+       }
+     });
+   }
+ });
+
+
+});
