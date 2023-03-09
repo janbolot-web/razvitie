4e5863b707b687a632b1958e3bc9e7171eea31e1
Janbolot B <janbolotcode@gmail.com>
2023-03-09 13:49:27 +0600

    image change

---
+++ wp-content/themes/Impreza/js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  zoom: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+var swiper1 = new Swiper(".mySwiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
